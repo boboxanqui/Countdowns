40122d1d4eaff28cb439420e85a5130b57a58fe5
Marcos Chanca <daxter2424@hotmail.com>
2024-05-08 19:44:03 +0200

    Add: time description for each time-slot

--- src/app/service/countdown.service.ts
+++ src/app/service/countdown.service.ts
@@ -12,7 +12,7 @@ export class CountdownService {
 
   testCountdown: Countdown = {
     creationDate: new Date(),
-    date: new Date(2024,4,10),
+    date: new Date(2024,4,13),
     id: 1,
     name: 'Contador de prueba',
     caption: 'Descripción de prueba para el primer contador de prueba de la historia del mundo mundial.',
